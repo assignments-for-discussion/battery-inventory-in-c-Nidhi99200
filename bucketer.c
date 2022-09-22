d28fd993f1eaf0222e303557085e085eeac464e9
Nidhi <93935886+Nidhi99200@users.noreply.github.com>
2022-09-22 17:42:17 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -9,14 +9,18 @@ struct CountsByUsage {
 
 struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
   struct CountsByUsage counts = {0, 0, 0};
+  //Iterate on total number of batteries
   for(int i=0;i<nBatteries;i++)
   {
+      //This condition checks if the charge cycle for a battery is less than 410
       if(cycles[i]<410)
-        counts.lowCount=counts.lowCount+1;
+        counts.lowCount=counts.lowCount+1; //Increment the count of the low charge battery by 1
+     //This condition checks if the charge cycle for a battery is between than 410 and 909
       else if(cycles[i]>=410 && cycles[i]<=909)
-        counts.mediumCount=counts.mediumCount+1;
+        counts.mediumCount=counts.mediumCount+1; //Increment the count of the medium charge battery by 1
+    //This condition checks if the charge cycle for a battery is greater than or equal to 910
       else
-        counts.highCount=counts.highCount+1;
+        counts.highCount=counts.highCount+1; //Increment the count of the high charge battery by 1
   }
   return counts;
 }
